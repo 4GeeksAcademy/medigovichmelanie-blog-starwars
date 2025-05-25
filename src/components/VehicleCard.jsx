8944261f6ae43a73e31309e972d2db37c93a5d6c
Melanie Dana Medigovich <melacem@gmail.com>
2025-05-25 15:04:15 +0000

    star wars api

---
+++ src/components/VehicleCard.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+
+export const VehicleCard = ({ vehicle }) => {
+    const [detalle, setDetalle] = useState({});
+    const [imgError, setImgError] = useState(false);
+
+    const getDetails = async () => {
+        const response = await fetch(`https://www.swapi.tech/api/vehicles/${vehicle.uid}`);
+        const data = await response.json();
+        setDetalle(data.result.properties);
+    }
+
+    const handleImageError = () => {
+        setImgError(true);
+    };
+
+    useEffect(() => {
+        getDetails();
+    }, [])
+
+    return (
+        <div className="col-md-3 col-lg-2 col-sm-6 col-12">
+            <div className="card">
+                {imgError ? (
+                    <div className="image-placeholder" style={{
+                        height: "200px",
+                        backgroundColor: "#f0f0f0",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        borderBottom: "1px solid #ddd"
+                    }}>
+                        <span className="text-muted">Imagen no disponible</span>
+                    </div>
+                ) : (
+                    <img
+                        src={`https://github.com/tbone849/star-wars-guide/blob/master/build/assets/img/vehicles/${vehicle?.uid}.jpg?raw=true`}
+                        className="card-img-top" 
+                        alt={`Imagen de ${vehicle?.name}`}
+                        onError={handleImageError}
+                        style={{ height: "200px", objectFit: "cover" }}
+                    />
+                )}
+                
+                <div className="card-body">
+                    <h5 className="card-title">{vehicle?.name}</h5>
+                    <p className="card-text">Model: {detalle.model}</p>
+                    <p className="card-text">Passengers: {detalle.passengers}</p>
+                    <p className="card-text">Vehicle Class: {detalle.vehicle_class}</p>
+                    <a href="#" className="btn btn-primary mt-2">Learn more!</a>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,68 @@
-import rigoImageUrl from "../assets/img/rigo-baby.jpg";
-import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import { useEffect, useState } from "react";
+import { CharacterCard } from "../components/CharacterCard";
+import { PlanetCard } from "../components/PlanetCard";
+import { VehicleCard } from "../components/VehicleCard";
+import useGlobalReducer from "../hooks/useGlobalReducer";
 
 export const Home = () => {
 
-  const {store, dispatch} =useGlobalReducer()
+    const { store, dispatch } = useGlobalReducer()
+    const [characters, setCharacters] = useState([])
+    const [planets, setPlanets] = useState([])
+    const [vehicles, setVehicles] = useState([])
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-		</div>
-	);
-}; 
+    const getCharacters = async () => {
+        const response = await fetch("https://www.swapi.tech/api/people/")
+        const data = await response.json()
+        setCharacters(data.results)
+    }
+
+    const getPlanets = async () => {
+        const response = await fetch("https://www.swapi.tech/api/planets/")
+        const data = await response.json()
+        setPlanets(data.results)
+    }
+
+    const getVehicles = async () => {
+        const response = await fetch("https://www.swapi.tech/api/vehicles/")
+        const data = await response.json()
+        setVehicles(data.results)
+    }
+
+    useEffect(() => {
+        getCharacters()
+        getPlanets()
+        getVehicles()
+    }, [])
+
+    return (
+        <div className="text-center mt-5">
+            <h1>Characters</h1>
+            <div className="d-flex overflow-x-scroll">
+                {
+                    characters.map(character => (
+                        <CharacterCard key={character.uid} character={character} />
+                    ))
+                }
+            </div>
+
+            <h1>Planets</h1>
+            <div className="d-flex overflow-x-scroll">
+                {
+                    planets.map(planet => (
+                        <PlanetCard key={planet.uid} planet={planet} />
+                    ))
+                }
+            </div>
+
+            <h1>Vehicles</h1>
+            <div className="d-flex overflow-x-scroll pb-3">
+                {
+                    vehicles.map(vehicle => (
+                        <VehicleCard key={vehicle.uid} vehicle={vehicle} />
+                    ))
+                }
+            </div>
+        </div>
+    );
+};
